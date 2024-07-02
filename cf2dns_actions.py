14bcf98e24e4109b722e727662aff02fd9c9b843
ZhaoJunnNie <22S104137@stu.hit.edu.cn>
2024-07-02 14:37:57 +0800

    Update cf2dns_actions.py

--- cf2dns_actions.py
+++ cf2dns_actions.py
@@ -14,7 +14,7 @@ import sys
 KEY = os.environ["KEY"]  #"o1zrmHAF"
 #CM:移动 CU:联通 CT:电信 AB:境外 DEF:默认
 #修改需要更改的dnspod域名和子域名
-print(os.environ["DOMAINS"])
+
 DOMAINS = json.loads(os.environ["DOMAINS"])  #{"hostmonit.com": {"@": ["CM","CU","CT"], "shop": ["CM", "CU", "CT"], "stock": ["CM","CU","CT"]},"4096.me": {"@": ["CM","CU","CT"], "vv": ["CM","CU","CT"]}}
 #腾讯云后台获取 https://console.cloud.tencent.com/cam/capi
 SECRETID = os.environ["SECRETID"]    #'AKIDV**********Hfo8CzfjgN'
